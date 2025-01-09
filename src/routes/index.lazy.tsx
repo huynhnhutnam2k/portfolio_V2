9b60862c4bea90f9fbaca67ca6e10cf27aa31d71
Hnam <hnam4527@gmail.com>
2025-01-09 23:28:16 +0700

    first commit

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,32 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import Welcome from "../components/Welcome";
+import Navbar from "../components/Navbar";
+import AnimatedBackground from "../components/AnimatedBackground";
+import Home from "../components/Home";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  const [showWelcome, setShowWelcome] = useState(true);
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome ? (
+          <Welcome onLoadingComplete={() => setShowWelcome(false)} />
+        ) : null}
+      </AnimatePresence>
+
+      {!showWelcome ? (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+        </>
+      ) : null}
+    </>
+  );
+}
